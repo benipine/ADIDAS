0fd91d54b7f8237bcbe2410fc8be645683a42895
JaeSungSim <jesung830@naver.com>
2022-12-13 23:51:33 +0900

    before merge

--- GUI/ADIDAS.py
+++ GUI/ADIDAS.py
@@ -9,6 +9,29 @@ from PyQt5.QtCore import *
 from PyQt5 import QtWidgets
 from PyQt5.uic import *
 
+class LoginWindow(QMainWindow):
+    def __init__(self):
+        super(LoginWindow, self).__init__()
+        loadUi("LoginWindow.ui", self)
+        
+        self.button_login.clicked.connect(self.loginclick)
+        
+    def loginclick(self):
+        login_id = self.input_id.text()
+        login_pw = self.input_pw.text()
+        id_list = ["KAFA04"]
+        pw_list = ["1234"]
+        if login_id not in id_list:
+            self.loginwrong()
+        else:
+            if login_pw != pw_list[id_list.index(login_id)]:
+                self.loginwrong()
+            else:
+                widget.setCurrentIndex(widget.currentIndex()+1)
+    
+    def loginwrong(self):
+        QMessageBox.critical(self, 'Login Failed', 'ID or PASSWORD is wrong')
+        
 class ImageThread(QThread):
     new_image = pyqtSignal(str)
     def __init__(self, parent=None):
@@ -24,39 +47,33 @@ class ImageThread(QThread):
                 fname = result_new.replace(result_old, "").strip()
                 result_old = result_new
                 self.new_image.emit(fname)
-                
-class LoginWindow(QMainWindow):
-    def __init__(self):
-        super(LoginWindow, self).__init__()
-        loadUi("LoginWindow.ui", self)
-        
-        self.button_login.clicked.connect(self.loginclick)
-        
-    def loginclick(self):
-        login_id = self.input_id.text()
-        print(login_id)
-        login_pw = self.input_pw.text()
-        print(login_pw)
-        id_list = ["KAFA04"]
-        pw_list = ["1234"]
-        if login_id not in id_list:
-            self.loginwrong()
-        else:
-            if login_pw != pw_list[id_list.index(login_id)]:
-                self.loginwrong()
-            else:
-                widget.setCurrentIndex(widget.currentIndex()+1)
-    
-    def loginwrong(self):
-        QMessageBox.critical(self, 'Login Failed', 'ID or PASSWORD is wrong')
         
 class LogWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi("LogWindow.ui", self)
         
-        vbox = QVBoxLayout()
-        self.scroll_area.addLayout(vbox)
+        self.scroll_log.setWidgetResizable(True)
+        self.inner = QFrame(self.scroll_log)
+        self.inner.setLayout(QVBoxLayout())
+        self.scroll_log.setWidget(self.inner)
+        
+        for i in range(0, 10):
+            labelname = "label"+str(i)
+            label=QLabel(objectName=labelname)
+            label.setAlignment(Qt.AlignCenter)
+            label.setMinimumSize(1, 30)
+            label.setMaximumSize(100000, 30)
+            label.setStyleSheet("font: 75 12pt 'PibotoLt';"
+"background-color: rgb(255, 255, 255)")
+            label.mousePressEvent = self.tostatus
+            label.setText("Log # 225"+" "*80+"2022-10-01 09:22:08"+" "*80+"Drone")
+            self.inner.layout().addWidget(label)
+            
+        select = self.findChild(QLabel, "label0")
+        select.setStyleSheet("font: 75 20pt 'PibotoLt';"
+"background-color: rgb(255, 255, 255)")
+
         self.button_logs.clicked.connect(self.tostatus)
         self.check_image = ImageThread()
         self.check_image.start()
@@ -68,13 +85,9 @@ class LogWindow(QMainWindow):
     def new_image(self, fname):
         self.image_before.setPixmap(QtGui.QPixmap("log/"+fname))
         self.image_before.repaint()
-        
         self.log_title.setText(fname)
         
-        self.add_log("1", fname, "phantom")
-        
     def add_log(self, num, time, dronetype):
-        label = QLabel(time)
         
 class StatWindow(QMainWindow):
     def __init__(self):
