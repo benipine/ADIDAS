e84e315d10e7c408506f5fb95dc0cfdbbc81b7f4
parkthepilot <parkthepilot@gmail.com>
2022-12-14 16:27:59 +0900

    temp: graph.py

--- Client/client.py
+++ Client/client.py
@@ -14,7 +14,7 @@ while True:
 
   else:
     file_name = file_list[len(file_list) // 2]
-    os.system("sshpass -p abcdefg scp /var/lib/motion/" + file_name + " pi@192.168.112.10:~/Desktop")
+    os.system("sshpass -p abcdefg scp /var/lib/motion/" + file_name + " pi@192.168.112.10:~/adidas/ADIDAS/GUI/log")
     time.sleep(5)
     print("> Transfered " + file_name)
 
